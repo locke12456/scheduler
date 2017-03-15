bd9d678af5159faf849dcd697f9cd77b28265150
Locke Chen <locke12456@gmail.com>
2017-03-15 17:25:27 +0800

    added extern c to support cpp files.

---
+++ tests/extern_c.h
@@ -0,0 +1,16 @@
+#ifndef __EXTERN_C__
+#define __EXTERN_C__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "clar.h"
+	void test_multi_task__make_sure_math_still_works(void);
+	void test_multi_task__initialize(void);
+	void test_multi_task__cleanup(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !1
